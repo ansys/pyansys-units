366b9a465c443c13e14b366d95ece4fb6ad1c1b9
Alexander Carvalho <132941110+acarvalh-work@users.noreply.github.com>
2023-08-16 10:23:35 -0400

    maint: deploy pyunits (#22)
    * fix: requirements and doc deploy
    
    * fix: doc build
    
    * fix: doc build
    
    * fix: src docs readme
    
    * fix: setup and manifest

--- src/ansys/units/__init__.py
+++ src/ansys/units/__init__.py
@@ -2,6 +2,7 @@
 
 pyunits
 """
+import os
 
 try:
     import importlib.metadata as importlib_metadata
@@ -16,3 +17,10 @@ from ansys.units.map import QuantityMap, QuantityMapError  # noqa: F401
 from ansys.units.quantity import Quantity, QuantityError  # noqa: F401
 from ansys.units.systems import UnitSystem, UnitSystemError  # noqa: F401
 from ansys.units.tables import UnitsTable  # noqa: F401
+
+_THIS_DIRNAME = os.path.dirname(__file__)
+_README_FILE = os.path.normpath(os.path.join(_THIS_DIRNAME, "docs", "README.rst"))
+
+if os.path.exists(_README_FILE):
+    with open(_README_FILE, encoding="utf8") as f:
+        __doc__ = f.read()
