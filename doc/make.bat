51b56c26e235ad87dbc1d07c637a09cde649a2db
Dan Williams <dan.williams@ansys.com>
2023-08-16 12:19:54 -0400

    Make warnings into errors

--- doc/make.bat
+++ doc/make.bat
@@ -9,7 +9,7 @@ if "%SPHINXBUILD%" == "" (
 )
 set SOURCEDIR=source
 set BUILDDIR=_build
-set SPHINXOPTS=-j auto -w build_errors.txt -N -q
+set SPHINXOPTS=-j auto -w build_errors.txt -N -q -W --keep-going
 
 if "%1" == "" goto help
 if "%1" == "clean" goto clean
