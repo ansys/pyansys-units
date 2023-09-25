6c4a7bc4a466bac744f03225ee0596511e518f88
Adam Boutin <adam.boutin@ansys.com>
2023-09-25 12:43:44 -0400

    Conflict fixed.

--- src/ansys/units/dimensions.py
+++ src/ansys/units/dimensions.py
@@ -32,7 +32,8 @@ class Dimensions:
     """
 
     def __init__(self, dimensions_container: dict[BaseDimensions : int | float] = None):
-        self._dimensions = dimensions_container.copy() if dimensions_container else {}
+        dimensions_container = dimensions_container or {}
+        self._dimensions = dimensions_container.copy()
         for x, y in dimensions_container.items():
             if not isinstance(x, BaseDimensions):
                 raise DimensionsError.INCORRECT_DIMENSIONS()
