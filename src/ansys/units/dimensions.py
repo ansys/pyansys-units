23b8a6642b1d7a51b660b34746acfcf91e154492
Adam Boutin <adam.boutin@ansys.com>
2023-10-11 09:11:16 -0400

    single dimension returns BaseDimension

--- src/ansys/units/dimensions.py
+++ src/ansys/units/dimensions.py
@@ -51,7 +51,7 @@ class Dimensions:
     @property
     def single_dimension(self):
         """Get the name of the base dimension."""
-        dims = [x.name for x in self._dimensions.keys()]
+        dims = [x for x in self._dimensions]
         if len(dims) != 1:
             raise DimensionsError.MULTIPLE_BASE_DIMENSIONS(dimensions=self)
         return dims[0]
