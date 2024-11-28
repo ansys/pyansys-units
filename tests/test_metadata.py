f5175c1dce61599d4b3f0adb69b51d9295263a61
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2024-11-28 11:59:20 +0100

    chore: bump version 0.4.dev0

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -24,4 +24,13 @@ from ansys.units import __version__
 
 
 def test_pkg_version():
-    assert __version__ == "0.3.3"
+    try:
+        import importlib.metadata as importlib_metadata
+    except ModuleNotFoundError:  # pragma: no cover
+        import importlib_metadata
+
+    # Read from the pyproject.toml
+    # major, minor, patch
+    read_version = importlib_metadata.version("ansys-units")
+
+    assert __version__ == read_version
