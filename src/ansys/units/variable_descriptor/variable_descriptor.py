9156c0d0f8c419b81302608cbcde3ba92ff08349
ANSYS\spearson <sean.pearson@ansys.com>
2025-05-09 11:10:02 +0100

    clean-up

--- src/ansys/units/variable_descriptor/variable_descriptor.py
+++ src/ansys/units/variable_descriptor/variable_descriptor.py
@@ -134,80 +134,35 @@ class VariableCatalog:
 # Add custom descriptors
 _D = QuantityDimensions
 variables = [
-    # pressure
-    ("ABSOLUTE_PRESSURE", _D.PRESSURE),
-    ("DYNAMIC_PRESSURE", _D.PRESSURE),
-    # ("STATIC_PRESSURE", _D.PRESSURE),
-    ("TOTAL_PRESSURE", _D.PRESSURE),
-    ("PRESSURE_COEFFICIENT", Dimensions()),
     # velocity
-    ("AXIAL_VELOCITY", _D.VELOCITY),
-    ("CELL_CONVECTIVE_COURANT_NUMBER", Dimensions()),
-    ("CELL_REYNOLDS_NUMBER", Dimensions()),
     # The dominant meaning of helicity comes from
     # particle physics where it is dimensionless.
     # In CFD, where it has a different
-    # meaning, it usually has dimension L^2T^-2.
-    # In Fluent it is LT^-2.
+    # meaning, it usually has dimension L^2 T^-2.
+    # In Fluent it is L T^-2.
     ("FLUENT_FLUID_HELICITY", _D.ACCELERATION),
     # Lambda 2 criterion is documented as dimensionless
     # but is T^-2 in Fluent.
     ("FLUENT_LAMBDA_2_CRITERION", _D.TIME**-2),
-    ("MESH_VELOCITY", _D.VELOCITY),
-    ("MESH_VELOCITY_MAG", _D.VELOCITY),
+    ("MESH_VELOCITY_MAGNITUDE", _D.VELOCITY),
     ("MESH_VELOCITY_X", _D.VELOCITY),
     ("MESH_VELOCITY_Y", _D.VELOCITY),
     ("MESH_VELOCITY_Z", _D.VELOCITY),
     # What Fluent calls the raw Q-criterion (to distinguish from
     # normalized) is just the Q-criterion so we omit the raw part.
-    ("Q_CRITERION", _D.TIME**-2),
-    ("Q_CRITERION_NORMALIZED", Dimensions()),
-    ("RADIAL_VELOCITY", _D.VELOCITY),
-    ("TANGENTIAL_VELOCITY", _D.VELOCITY),
     ("VELOCITY_ANGLE", _D.ANGLE),
     ("VELOCITY_MAGNITUDE", _D.VELOCITY),
     ("VELOCITY_X", _D.VELOCITY),
     ("VELOCITY_Y", _D.VELOCITY),
     ("VELOCITY_Z", _D.VELOCITY),
-    ("VORTICITY", _D.TIME**-1),
-    ("VORTICITY_MAG", _D.TIME**-1),
+    ("VORTICITY_MAGNITUDE", _D.TIME**-1),
     ("VORTICITY_X", _D.TIME**-1),
     ("VORTICITY_Y", _D.TIME**-1),
     ("VORTICITY_Z", _D.TIME**-1),
     # density
     ("FLUENT_FLUID_DENSITY_ALL", _D.DENSITY),
-    # properties
-    # consider moving to the quantity_dimensions.
-    ("PRANDTL_NUMBER", Dimensions()),
-    # turbulence
-    ("EFFECTIVE_PRANDTL_NUMBER", Dimensions()),
-    ("EFFECTIVE_THERMAL_CONDUCTIVITY", _D.THERMAL_CONDUCTIVITY),
-    ("EFFECTIVE_VISCOSITY", _D.DYNAMIC_VISCOSITY),
-    ("PRODUCTION_OF_K", _D.MASS * _D.LENGTH**-1 * _D.TIME**-3),
-    ("SPECIFIC_DISSIPATION_RATE", _D.TIME**-1),
-    ("TURBULENT_DISSIPATION_RATE", _D.LENGTH**2 * _D.TIME**3),
-    ("TURBULENT_INTENSITY", Dimensions()),
-    ("TURBULENT_VISCOSITY", _D.DYNAMIC_VISCOSITY),
-    ("TURBULENT_VISCOSITY_RATIO", Dimensions()),
-    ("TURBULENT_REYNOLDS_NUMBER", Dimensions()),
-    ("TURBULENT_KINETIC_ENERGY", _D.LENGTH**2 * _D.TIME**-2),
-    ("WALL_Y_PLUS", Dimensions()),
-    ("WALL_Y_STAR", Dimensions()),
     # wall fluxes
-    (
-        "SURFACE_HEAT_TRANSFER_COEFFICIENT",
-        _D.POWER * _D.LENGTH**-2 * _D.TEMPERATURE**-1,
-    ),
-    ("SKIN_FRICTION_COEFFICIENT", Dimensions()),
-    ("SURFACE_HEAT_FLUX", _D.HEAT_FLUX_DENSITY),
-    ("SURFACE_NUSSELT_NUMBER", Dimensions()),
-    ("SURFACE_STANTON_NUMBER", Dimensions()),
-    (
-        "WALL_ADJACENT_HEAT_TRANSFER_COEFFICIENT",
-        _D.POWER * _D.LENGTH**-2 * _D.TEMPERATURE**-1,
-    ),
-    ("WALL_SHEAR_STRESS", _D.STRESS),
-    ("WALL_SHEAR_STRESS_MAG", _D.STRESS),
+    ("WALL_SHEAR_STRESS_MAGNITUDE", _D.STRESS),
     ("WALL_SHEAR_STRESS_X", _D.STRESS),
     ("WALL_SHEAR_STRESS_Y", _D.STRESS),
     ("WALL_SHEAR_STRESS_Z", _D.STRESS),
@@ -219,32 +174,23 @@ variables = [
     ("MASS_IMBALANCE", _D.MASS * _D.TIME**-1),
     # derivatives
     ("PRESSURE_HESSIAN_INDICATOR", Dimensions()),
-    ("STRAIN_RATE", _D.TIME**-1),
     ("DVELOCITY_DX", _D.TIME**-1),
-    ("DVELOCITY_DX_MAG", _D.TIME**-1),
+    ("DVELOCITY_DX_MAGNITUDE", _D.TIME**-1),
     ("DVELOCITY_DX_X", _D.TIME**-1),
     ("DVELOCITY_DX_Y", _D.TIME**-1),
     ("DVELOCITY_DX_Z", _D.TIME**-1),
     ("DVELOCITY_DY", _D.TIME**-1),
-    ("DVELOCITY_DY_MAG", _D.TIME**-1),
+    ("DVELOCITY_DY_MAGNITUDE", _D.TIME**-1),
     ("DVELOCITY_DY_X", _D.TIME**-1),
     ("DVELOCITY_DY_Y", _D.TIME**-1),
     ("DVELOCITY_DY_Z", _D.TIME**-1),
     ("DVELOCITY_DZ", _D.TIME**-1),
-    ("DVELOCITY_DZ_MAG", _D.TIME**-1),
+    ("DVELOCITY_DZ_MAGNITUDE", _D.TIME**-1),
     ("DVELOCITY_DZ_X", _D.TIME**-1),
     ("DVELOCITY_DZ_Y", _D.TIME**-1),
     ("DVELOCITY_DZ_Z", _D.TIME**-1),
-    # thermodynamics
-    ("SPECIFIC_ENTHALPY", _D.ENTHALPY / _D.MASS),
-    ("SPECIFIC_ENTROPY", _D.ENTROPY / _D.MASS),
-    ("SPECIFIC_INTERNAL_ENERGY", _D.ENERGY / _D.MASS),
-    ("SPECIFIC_TOTAL_ENERGY", _D.ENERGY / _D.MASS),
     # mesh
-    ("ABSOLUTE_ANGULAR_COORDINATE", _D.ANGLE),
-    ("ANGULAR_COORDINATE", _D.ANGLE),
     ("ANISOTROPIC_ADAPTION_CELLS", Dimensions()),
-    ("AXIAL_COORDINATE", _D.LENGTH),
     ("BOUNDARY_CELL_DISTANCE", Dimensions()),
     ("BOUNDARY_LAYER_CELLS", Dimensions()),
     ("BOUNDARY_NORMAL_DISTANCE", Dimensions()),
@@ -261,12 +207,6 @@ variables = [
     ("FACE_HANDEDNESS", Dimensions()),
     ("INTERFACE_OVERLAP_FRACTION", Dimensions()),
     ("MARK_POOR_ELEMENTS", Dimensions()),
-    ("COORDINATE", _D.LENGTH),
-    ("COORDINATE_MAG", _D.LENGTH),
-    ("COORDINATE_X", _D.LENGTH),
-    ("COORDINATE_Y", _D.LENGTH),
-    ("COORDINATE_Z", _D.LENGTH),
-    ("RADIAL_COORDINATE", _D.LENGTH),
     ("SMOOTHED_CELL_REFINE_LEVEL", Dimensions()),
     ("X_FACE_AREA", _D.AREA),
     ("Y_FACE_AREA", _D.AREA),
