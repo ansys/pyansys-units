33d4ddc3a46f53d9bc4dbcaed78f4a75ff14f235
James Hilton-Balfe <gobot1234yt@gmail.com>
2025-11-05 16:05:21 +0000

    Add hints for unit_registry

--- src/ansys/units/unit_registry.py
+++ src/ansys/units/unit_registry.py
@@ -21,7 +21,9 @@
 # SOFTWARE.
 """Provides the ``UnitRegistry`` class."""
 
+from collections.abc import Generator
 import os
+from typing import Any
 
 import yaml
 
@@ -53,7 +55,7 @@ class UnitRegistry:
     >>> ureg.foot_per_sec = fps
     """
 
-    def __init__(self, config="cfg.yaml", other: dict = None):
+    def __init__(self, config: str = "cfg.yaml", other: dict[str, Unit] | None = None):
         unitdict = other or {}
 
         if config:
@@ -76,15 +78,17 @@ class UnitRegistry:
         for key in attrs:
             returned_string += f"{key}, "
         return returned_string
+    
+    if TYPE_CHECKING:
+        def __getattr__(self, name: str) -> Unit: ...
 
-    def __setattr__(self, name: str, unit: any) -> None:
+    def __setattr__(self, name: str, unit: Any) -> None:
         if hasattr(self, name):
             raise UnitAlreadyRegistered(name)
         self.__dict__[name] = unit
 
-    def __iter__(self):
-        for item in self.__dict__:
-            yield getattr(self, item)
+    def __iter__(self) -> Generator[str]:
+        yield from self.__dict__
 
 
 class UnitAlreadyRegistered(ValueError):
